0bcfd7da150f4812c6ad27bcf079547120d5e378
liwenxin <mxlwx@126.com>
2019-11-05 23:36:43 +0800

    update decorators

--- src/example/decorators.ts
+++ src/example/decorators.ts
@@ -50,12 +50,14 @@
 // const d = new ClassD()
 // console.log(d.name) // 输出 xxlai
 
+// 返回装饰器包括的类属性，会合并
 // function classDecorator<T extends new (...args: any[]) => {}>(target: T) {
 //   return class extends target {
 //     public newProperty = 'new property'
 //     public hello = 'override'
 //   }
 // }
+// 只返回装饰器的类属性，不会合并，会覆盖原先的类定义
 // function classDecorator(target: any): any {
 //   return class {
 //     public newProperty = 'new property'
@@ -82,90 +84,104 @@ let obj12: ObjWithAnyKeys = {
   age: 18,
 }
 Object.defineProperty(obj12, 'name', {
-  value: 'lison',
-  // writable: false,
+  value: 'jam',
+  // writable: false,  // 是否可写的
   writable: false,
-  // configurable: true,
+  // configurable: true,  // 是否可配置的
   configurable: false,
-  // enumerable: false,
+  // enumerable: false,  // 是否可枚举的
   enumerable: true,
 })
+// 设置了不可配置，不允许重复定义name属性
 // Object.defineProperty(obj12, 'name', {
-//     value: 'lison',
-//     writable: true,
+//   value: 'lison',
+//   writable: true
 // })
 // console.log(obj12.name)
 // obj12.name = 'test'
 // console.log(obj12.name)
 // for (const key in obj12) {
-//     console.log(key)
+//   console.log(key)
 // }
 
 // function enumerable(bool: boolean) {
-//     return (target: any, propertyName: string, descriptor: PropertyDescriptor) => {
-//         console.log(target, propertyName)
-//         descriptor.enumerable = bool
-//     }
+//   return (
+//     target: any,
+//     propertyName: string,
+//     descriptor: PropertyDescriptor
+//   ) => {
+//     console.log(target, propertyName)
+//     descriptor.enumerable = bool
+//   }
 // }
 // function enumerable(bool: boolean): any {
-//     return (target: any, propertyName: string, descriptor: PropertyDescriptor) => {
-//         return {
-//             value() {
-//                 return 'not age'
-//             },
-//             enumerable: bool,
-//         }
+//   return (
+//     target: any,
+//     propertyName: string,
+//     descriptor: PropertyDescriptor
+//   ) => {
+//     return {
+//       value() {
+//         return 'not age'
+//       },
+//       enumerable: bool
 //     }
+//   }
 // }
 // class ClassF {
-//     constructor(public age: number) {}
-//     @enumerable(false)
-//     public getAge() {
-//         return this.age
-//     }
+//   constructor(public age: number) {}
+//   @enumerable(false)
+//   public getAge() {
+//     return this.age
+//   }
 // }
 // const classF = new ClassF(18)
-// console.log(classF.getAge())
+// console.log(classF.getAge()) // 输出 not age
 // for (const key in classF) {
-//     console.log(key)
+//   console.log(key)
 // }
 
 // function enumerable(bool: boolean) {
-//     return (target: any, propertyName: string, descriptor: PropertyDescriptor) => {
-//         descriptor.enumerable = bool
-//     }
+//   return (
+//     target: any,
+//     propertyName: string,
+//     descriptor: PropertyDescriptor
+//   ) => {
+//     descriptor.enumerable = bool
+//   }
 // }
 // class ClassG {
-//     private _name: string
-//     constructor(name: string) {
-//         this._name = name
-//     }
-//     @enumerable(true)
-//     get name() {
-//         return this._name
-//     }
-//     set name(name) {
-//         this._name = name
-//     }
+//   private _name: string
+//   constructor(name: string) {
+//     this._name = name
+//   }
+//   @enumerable(true)
+//   get name() {
+//     return this._name
+//   }
+//   set name(name) {
+//     this._name = name
+//   }
 // }
-// const classG = new ClassG('lison')
+// const classG = new ClassG('jam')
 // for (const key in classG) {
-//     console.log(key)
+//   console.log(key)
 // }
 
 // function printPropertyName(target: any, propertyName: string) {
-//     console.log(propertyName)
+//   console.log(propertyName)
 // }
 // class ClassH {
-//     @printPropertyName
-//     public name: string
+//   @printPropertyName
+//   public name: string
 // }
 
+// 参数装饰器
 function required(target: any, propertName: string, index: number) {
   console.log(`修饰的是${propertName}的第${index + 1}个参数`)
 }
 class ClassI {
-  public name: string = 'lison'
+  public name: string = 'jam'
   public age: number = 18
   public getInfo(prefix: string, @required infoType: string): any {
     return prefix + ' ' + this[infoType]
